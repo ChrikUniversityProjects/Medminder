d6d349c4d1e6c9e5891e7990dbbc4bbb6d40a71f
Chris Kelly <chrik@chriss-macbook-pro.local>
2018-02-19 14:56:21 -0800

    Initial Commit

---
+++ Medminder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Medminder
+//
+//  Created by Chris Kelly on 2/19/18.
+//  Copyright © 2018 ck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
